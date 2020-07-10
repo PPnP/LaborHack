3e57f41ff4ef8e06269cb69a8292c27a0dd7e47c
Stepan Denisov <sd.denisoff@gmail.com>
2020-07-10 18:14:26 +0300

    replacing name to title

--- app/api/controllers/locations.py
+++ app/api/controllers/locations.py
@@ -1,12 +1,22 @@
-from flask import render_template
-from flask.views import MethodView
+from flask import render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
 from flask_login import current_user
+
 from config import yandex_api_key
+from app.api.controllers.form import FormController
+from app.api.forms.event import AddEventForm
 
 
-class LocationsController(MethodView):
+class LocationsController(FormController):
+    def get_form(self) -> FlaskForm:
+        return AddEventForm()
+
     def get(self):
         user = None
         if current_user.is_authenticated:
             user = current_user
         return render_template('locations.html', yandex_api_key=yandex_api_key, user=user)
+
+    def post(self):
+        flash('Заявка успешно отправлена', 'success')
+        return redirect(url_for('api.Locations'))
---
+++ app/api/forms/event.py
@@ -0,0 +1,7 @@
+from app.api.forms import *
+
+
+class AddEventForm(FlaskForm):
+    pass
+    # title = PasswordField(label='Пароль', validators=[InputRequired(message='Заполните все обязательные поля')])
+    # submit = SubmitField('Войти')
