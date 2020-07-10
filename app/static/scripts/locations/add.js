cd72e9ff0f1e45ffed1834ebea5ef815f81c89c2
Matvey Kottsov <matvey@kottsov.com>
2020-07-10 22:56:14 +0300

    to refactor add js

--- app/static/scripts/locations/add.js
+++ app/static/scripts/locations/add.js
@@ -3,21 +3,26 @@ const map = document.getElementById("map");
 const addBlock = document.querySelector(".add-event");
 const backButton = document.querySelector(".back-button");
 
+const hideForm = () => {
+    // Возвращаем карту и кнопку добавления мероприятия
+    map.style.display = "block";
+    addButton.style.display = "block";
+
+    // Скрываем форму добавления мероприятия и кнопку назад
+    addBlock.classList.remove("add-event_visible");
+    backButton.classList.remove("back-button_visible");
+};
+
 const showForm = () => {
+    // Скрываем карту и кнопку добавления мероприятия
     map.style.display = "none";
     addButton.style.display = "none";
 
+    // Показываем форму добавления мероприятия и кнопку назад
     addBlock.classList.add("add-event_visible");
-
     backButton.classList.add("back-button_visible");
-    backButton.addEventListener("click", () => {
-        map.style.display = "block";
-        addButton.style.display = "block";
 
-        addBlock.classList.remove("add-event_visible");
-        backButton.classList.remove("back-button_visible");
-
-    });
+    backButton.addEventListener("click", hideForm);
 };
 
 addButton.addEventListener("click", showForm);
