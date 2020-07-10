9bc57948dfe7c4be4d858203e48a5940ecca61ba
Matvey Kottsov <matvey@kottsov.com>
2020-07-10 22:56:08 +0300

    to add map filtering

--- app/static/scripts/locations/map.js
+++ app/static/scripts/locations/map.js
@@ -1,54 +1,108 @@
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-        center: [55.751574, 37.573856],
-        zoom: 9
-    }, {
-        searchControlProvider: 'yandex#search'
-    }),
+const categoryButtons = document.querySelectorAll(".category");
+var ymap;
+let activeFilter = null;
 
-        // Создаём макет содержимого.
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
+async function getPlaceJson(slug) {
+    const response = await fetch(`/data/${slug}`);
+    return await response.json();
+}
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'images/myIcon.gif',
-            // Размеры метки.
-            iconImageSize: [30, 42],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        }),
+const filterPlaces = (slug) => {
+    ymap.geoObjects.each((collection) => {
+        collection.each((obj) => {
+            let feature = collection.options._options.feature;
+            obj.options.set({ visible: slug == null || feature == slug });
+        })
+    });
 
-        myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-            hintContent: 'Собственный значок метки с контентом',
-            balloonContent: 'А эта — новогодняя',
-            iconContent: '12'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#imageWithContent',
-            // Своё изображение иконки метки.
-            iconImageHref: 'images/ball.png',
-            // Размеры метки.
-            iconImageSize: [48, 48],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-24, -24],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
-            iconContentOffset: [15, 15],
-            // Макет содержимого.
-            iconContentLayout: MyIconContentLayout
+    activeFilter = slug;
+}
+
+const activateButtons = () => {
+    categoryButtons.forEach(button => {
+        button.classList.remove("category_inactive");
+    });
+}
+
+const deactivateButtons = (except) => {
+    categoryButtons.forEach(button => {
+        if (button == except) {
+            return button.classList.remove("category_inactive");
+        }
+
+        button.classList.add("category_inactive");
+    });
+};
+
+const init = () => {
+    // Создание карты.    
+    ymap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [59.91, 30.50],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 12
+    });
+
+    loadPlacesJSON(ymap);
+
+    categoryButtons.forEach(button => {
+        button.addEventListener("click", () => categoryButtonClick(button));
+    });
+}
+
+const categoryButtonClick = (button) => {
+    let slug = button.getAttribute("data-slug");
+
+    if (activeFilter == slug) {
+        filterPlaces(null);
+        return activateButtons();
+    }
+
+    filterPlaces(slug);
+    deactivateButtons(button);
+};
+
+const drawPlace = (map, category, placeJSON) => {
+    let collection = new ymaps.GeoObjectCollection({}, {
+        feature: category
+    });
+
+    placeJSON.forEach(place => {
+        let obj = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    place.latitude,
+                    place.longitude
+                ]
+            }
         });
 
-    myMap.geoObjects
-        .add(myPlacemark)
-        .add(myPlacemarkWithContent);
-});
+        collection.add(obj);
+    });
+
+    map.geoObjects.add(collection);
+}
+
+const loadPlacesJSON = (map) => {
+    let categories = [
+        "culture",
+        "development",
+        "food",
+        "leisure",
+        "life",
+        "religion",
+    ];
+
+    categories.forEach(category => {
+        getPlaceJson(category).then((json) => {
+            drawPlace(map, category, json);
+        });
+    });
+}
+
+ymaps.ready(init);
