5e8e27148d19410775447f35ce0bf4bc6f210754
Matvey Kottsov <matvey@kottsov.com>
2020-07-11 19:19:38 +0300

    to add stay at home page

--- app/api/routes.py
+++ app/api/routes.py
@@ -25,5 +25,8 @@ api.add_url_rule('/education', view_func=EducationController.as_view('Education'
 from app.api.controllers.help import HelpController
 api.add_url_rule('/help', view_func=HelpController.as_view('Help'))
 
+from app.api.controllers.stayhome import StayHomeController
+api.add_url_rule('/stayhome', view_func=StayHomeController.as_view('StayHome'))
+
 from app.api.controllers.data import DataController
 api.add_url_rule('/data/<string:type>', view_func=DataController.as_view('Data'))
