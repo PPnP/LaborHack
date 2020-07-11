a656a4dddd3b1b860738bd7bc055775b77a2f01c
Stepan Denisov <sd.denisoff@gmail.com>
2020-07-11 13:31:22 +0300

    stayhome flash added

--- app/api/controllers/index.py
+++ app/api/controllers/index.py
@@ -1,7 +1,9 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, flash
 from flask.views import MethodView
 
 
 class IndexController(MethodView):
     def get(self):
+        flash('Оставайтесь дома. В связи с неблагоприятной эпидемиологической обстановкой '
+              'постарайтесь не выходить на улицу без необходимости. Это важно 🙂', 'info')
         return redirect(url_for('api.Locations'))
